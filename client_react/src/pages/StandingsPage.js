8eb2e6b3d1e5304fbc095164b70b430f003a6d33
ismakova <ismakova.dayana@mail.ru>
2021-05-25 18:57:42 +0900

    готова пагинация

--- client_react/src/pages/StandingsPage.js
+++ client_react/src/pages/StandingsPage.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {Container, Nav, Table} from "react-bootstrap";
 import '../styles.css';
 import {Link} from "react-router-dom";
@@ -6,6 +6,8 @@ import {MAIN_ROUTE} from "../utils/consts";
 import {Context} from "../index";
 import {fetchGamer, fetchSession} from "../http/space_x_api";
 import {observer} from "mobx-react-lite";
+import ReactPaginate from 'react-paginate';
+
 
 const StandingsPage = observer(() => {
     const {game} = useContext(Context)
@@ -14,6 +16,40 @@ const StandingsPage = observer(() => {
         fetchSession().then(data => game.setSessions(data.rows))
         fetchGamer().then(data => game.setGamers(data.rows))
     }, [])
+
+
+    const [pageNumber, setPageNumber] = useState(0)
+
+    const sessionsPerPage = 11
+    const pagesVisited = pageNumber * sessionsPerPage
+
+    console.log(game.sessions)
+
+    const displaySessions = game.sessions
+        .slice(pagesVisited, pagesVisited + sessionsPerPage)
+        .map((session) => {
+            return (
+                <tr>
+                    <td>{session.id}</td>
+                    {game.gamers.map(items => {
+                        console.log(items.id, session.gamerId)
+                        if (items.id === parseInt(session.gamerId))
+                            return <td>{items.nickname}</td>
+                    })
+                    }
+                    <td>{session.score}</td>
+                    <td>{session.time_session}</td>
+                </tr>
+            );
+        });
+
+    const pageCount = Math.ceil( game.sessions.length / sessionsPerPage);
+
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
+    };
+
+
     return (
         <Container className="d-flex justify-content-center"
                    style={{height: window.innerHeight - 70}}>
@@ -32,23 +68,22 @@ const StandingsPage = observer(() => {
                         </tr>
                         </thead>
                         <tbody>
-                        {game.sessions.map(session =>
-                            <tr>
-
-                                <td>{session.id}</td>
-                                {game.gamers.map(items => {
-                                    console.log(items.id, session.gamerId)
-                                    if (items.id === parseInt(session.gamerId))
-                                        return <td>{items.nickname}</td>
-                                })
-                                }
-                                <td>{session.score}</td>
-                                <td>{session.time_session}</td>
-                            </tr>
-                        )}
+                        {displaySessions}
                         </tbody>
                     </Table>
+
                 </div>
+                <ReactPaginate
+                    previousLabel={"Previous"}
+                    nextLabel={"Next"}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={"paginationBttns"}
+                    previousLinkClassName={"previousBttn"}
+                    nextLinkClassName={"nextBttn"}
+                    disabledClassName={"paginationDisabled"}
+                    activeClassName={"paginationActive"}
+                />
             </div>
         </Container>
     );
